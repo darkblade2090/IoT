05f3c4c4bc55b999667e4862c9a4a6a52cb7367c
darkblade2090 <84008691+darkblade2090@users.noreply.github.com>
2021-05-22 15:14:51 +0530

    Add files via upload

---
+++ SMTP Mail Server/Alerting user by Sending email when ans object is detected by IR sensor.py
@@ -0,0 +1,30 @@
+import smtplib
+def send_mail(myemail, password, sendto,subject, msg):
+    mailserver = smtplib.SMTP('smtp.gmail.com',587)
+    headers = ["From: " + myemail, "Subject: " + subject, "To: " + sendto,
+                   "MIME-Version: 1.0", "Content-Type: text/html"]
+    headers = "\r\n".join(headers)
+    print("Connecting...")
+    mailserver.starttls()
+    print("Logging In...")
+    mailserver.login(myemail , password)
+    print("Sending Email...")
+    mailserver.sendmail(myemail , sendto, msg)
+    print("Email Sent")
+    mailserver.quit()
+
+myemail = 'tech3zone139@gmail.com'
+password= 'Titanium@7456'
+sendto  = 'certificate1.softpro@gmail.com'
+msg = "Hello,\r\nObject Detected!!!"
+subject = "Alert from Raspberry Pi"
+from RPi import GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(40 , GPIO.IN)
+while(1):
+	if GPIO.input(40) == 1:
+		print("Object Detected!!!")
+		send_mail(myemail,password,sendto,subject,msg)
+	
